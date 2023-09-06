628e1895342531a8ed753213e8e5a8576629fbcb
Yousufgithubrit <142991789+Yousufgithubrit@users.noreply.github.com>
2023-09-06 16:04:47 +0530

    Add files via upload

---
+++ Diceegame-main/diceeGame-main/index.js
@@ -0,0 +1,15 @@
+var randomNumber1 = Math.ceil(Math.random()*6);
+document.querySelector(".img1").setAttribute("src", "images/dice"+randomNumber1+".png");
+
+var randomNumber2 = Math.ceil(Math.random()*6);
+document.querySelector(".img2").setAttribute("src", "images/dice"+randomNumber2+".png");
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").textContent = "🚩Player 1 Wins!"
+}
+else if (randomNumber1 < randomNumber2){
+  document.querySelector("h1").textContent = "Player 2 Wins 🚩!"
+}
+else {
+  document.querySelector("h1").textContent = "Refresh Me!"
+}
